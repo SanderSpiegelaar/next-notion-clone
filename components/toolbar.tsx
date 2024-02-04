23a4c917bf9c0d2c5d7063451c6d74cd71af702a
Sander Spiegelaar <sanderspiegelaar@gmail.com>
2024-02-04 17:40:55 +0100

    Added cover image functionality

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -3,12 +3,13 @@
 import { Doc } from "@/convex/_generated/dataModel";
 import { IconPicker } from "@/components/icon-picker";
 import { Button } from "@/components/ui/button";
-import TextAreaAutoSize from "react-textarea-autosize";
+import { api } from "@/convex/_generated/api";
 
 import { ImageIcon, Smile, X } from "lucide-react";
 import React, { ElementRef, useRef, useState } from "react";
 import { useMutation } from "convex/react";
-import { api } from "@/convex/_generated/api";
+import TextAreaAutoSize from "react-textarea-autosize";
+import { useCoverImage } from "@/hooks/use-cover-image";
 
 interface ToolbarProps {
   initialData: Doc<"documents">;
@@ -22,6 +23,8 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
 
   const update = useMutation(api.documents.update);
   const removeIcon = useMutation(api.documents.removeIcon);
+
+  const coverImage = useCoverImage();
 
   const enableInput = () => {
     if (preview) return;
@@ -100,7 +103,7 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
         )}
         {!initialData.coverImage && !preview && (
           <Button
-            onClick={() => {}}
+            onClick={coverImage.onOpen}
             className="text-muted-foreground text-xs"
             variant="outline"
             size="sm"
